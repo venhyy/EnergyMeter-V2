0f7920b301ca98a06b87dcb031fbdfd945849201
Jaroslav Venhoda <venhy1337@gmail.com>
2023-04-08 12:57:10 +0200

    added functions for 3 pzem

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <PZEM004Tv30.h>
 #include <AsyncTCP.h>
 #include <ArduinoJson.h>
+#include <HardwareSerial.h>
 
-const char* ssid = "ASUS";
-const char* password = "asus1337";
+const char *ssid = "ASUS";
+const char *password = "asus1337";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -15,16 +16,27 @@ AsyncWebServer server(80);
 // Create AsyncWebSocket object on port 81
 AsyncWebSocket ws("/ws");
 
+#define MAX_PZEM 3
+#define PZEM_DEFAULT_TIMEOUT 1000
+unsigned long lastMeasurementTime[MAX_PZEM];
 
-void sendMeasurementData() {
-  if (ws.count() == 0) {
+PZEM004Tv30 pzem[MAX_PZEM] = {
+    PZEM004Tv30(Serial2, 16, 17),
+    PZEM004Tv30(Serial2, 18, 19),
+    PZEM004Tv30(Serial2, 20, 21)};
+
+void sendMeasurementData()
+{
+  if (ws.count() == 0)
+  {
     return;
   }
-  
+
   StaticJsonDocument<300> doc;
   JsonArray arr = doc.to<JsonArray>();
-  
-  for (int i = 0; i < 3; i++) {
+
+  for (int i = 0; i < 3; i++)
+  {
     JsonObject obj = arr.createNestedObject();
     obj["voltage"] = random(220, 240);
     obj["current"] = random(5, 15) / 10.0;
@@ -40,41 +52,56 @@ void sendMeasurementData() {
   ws.textAll(jsonStr);
 }
 
-
 // Define function to handle WebSocket events
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-  if (type == WS_EVT_CONNECT) {
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
+  {
     Serial.println("WebSocket client connected");
     client->ping();
-  } else if (type == WS_EVT_DISCONNECT) {
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
     Serial.println("WebSocket client disconnected");
-  } else if (type == WS_EVT_ERROR) {
-    Serial.printf("WebSocket error(%s): %s\n", (const char*)arg, (char*)data);
-  } else if (type == WS_EVT_DATA) {
+  }
+  else if (type == WS_EVT_ERROR)
+  {
+    Serial.printf("WebSocket error(%s): %s\n", (const char *)arg, (char *)data);
+  }
+  else if (type == WS_EVT_DATA)
+  {
     Serial.print("WebSocket data received: ");
-    for (size_t i = 0; i < len; i++) {
-      Serial.printf("%c", (char) data[i]);
+    for (size_t i = 0; i < len; i++)
+    {
+      Serial.printf("%c", (char)data[i]);
     }
     Serial.println();
   }
 }
 
-void serveIndexHtml(AsyncWebServerRequest *request) {
-  if (SPIFFS.exists("/index2.html")) {
+void serveIndexHtml(AsyncWebServerRequest *request)
+{
+  if (SPIFFS.exists("/index2.html"))
+  {
     AsyncWebServerResponse *response = request->beginResponse(SPIFFS, "/index2.html", "text/html");
     request->send(response);
-  } else {
+  }
+  else
+  {
     request->send(404);
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+  Serial2.begin(115200, SERIAL_8N1, 17, 16); // TX on pin 17, RX on pin 16
   SPIFFS.begin();
 
   // Connect to Wi-Fi network
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -87,16 +114,47 @@ void setup() {
   ws.onEvent(onWsEvent);
   server.addHandler(&ws);
   server.begin();
+
+  for (int i = 0; i < MAX_PZEM; i++)
+  {
+    lastMeasurementTime[i] = 0;
+  }
 }
 
-void loop() {
-
+void loop()
+{
   // Handle WebSocket events
   ws.cleanupClients();
 
   static unsigned long prevTime = 0;
-  if (millis() - prevTime >= 5000) {
-    sendMeasurementData();
+  if (millis() - prevTime >= 5000)
+  {
+    StaticJsonDocument<256> doc;
+    JsonArray arr = doc.to<JsonArray>();
+
+    for (int i = 0; i < 3; i++)
+    {
+      JsonObject obj = arr.createNestedObject();
+
+      float voltage = pzem[i].voltage();
+      float current = pzem[i].current();
+      float power = pzem[i].power();
+      float energy = pzem[i].energy();
+      float frequency = pzem[i].frequency();
+      float powerFactor = pzem[i].pf();
+
+      obj["voltage"] = voltage;
+      obj["current"] = current;
+      obj["power"] = power;
+      obj["frequency"] = frequency;
+      obj["pf"] = powerFactor;
+    }
+
+    String jsonStr;
+    serializeJson(doc, jsonStr);
+    Serial.print(jsonStr);
+    ws.textAll(jsonStr);
+    // sendMeasurementData();
     prevTime = millis();
   }
 }
