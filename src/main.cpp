99bd755638a64a2d79b094c760b192aee7fb57f0
Jaroslav Venhoda <venhy1337@gmail.com>
2023-04-08 11:23:34 +0200

    web with random data

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,62 @@
 #include <FS.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
+#include <PZEM004Tv30.h>
+#include <AsyncTCP.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "ASUS";
 const char* password = "asus1337";
 
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+
+// Create AsyncWebSocket object on port 81
+AsyncWebSocket ws("/ws");
+
+
+void sendMeasurementData() {
+  if (ws.count() == 0) {
+    return;
+  }
+  
+  StaticJsonDocument<300> doc;
+  JsonArray arr = doc.to<JsonArray>();
+  
+  for (int i = 0; i < 3; i++) {
+    JsonObject obj = arr.createNestedObject();
+    obj["voltage"] = random(220, 240);
+    obj["current"] = random(5, 15) / 10.0;
+    obj["power"] = random(2200, 24000) / 10.0;
+    obj["frequency"] = random(49, 51) * 1.00;
+    obj["pf"] = random(80, 100) / 100.0;
+  }
+
+  String jsonStr;
+  serializeJson(doc, jsonStr);
+
+  // Send the JSON string over the WebSocket connection
+  ws.textAll(jsonStr);
+}
+
+
+// Define function to handle WebSocket events
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+  if (type == WS_EVT_CONNECT) {
+    Serial.println("WebSocket client connected");
+    client->ping();
+  } else if (type == WS_EVT_DISCONNECT) {
+    Serial.println("WebSocket client disconnected");
+  } else if (type == WS_EVT_ERROR) {
+    Serial.printf("WebSocket error(%s): %s\n", (const char*)arg, (char*)data);
+  } else if (type == WS_EVT_DATA) {
+    Serial.print("WebSocket data received: ");
+    for (size_t i = 0; i < len; i++) {
+      Serial.printf("%c", (char) data[i]);
+    }
+    Serial.println();
+  }
+}
 
 void serveIndexHtml(AsyncWebServerRequest *request) {
   if (SPIFFS.exists("/index.html")) {
@@ -33,8 +84,19 @@ void setup() {
   // Serve index.html file
   server.on("/", HTTP_GET, serveIndexHtml);
 
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
   server.begin();
 }
 
 void loop() {
+
+  // Handle WebSocket events
+  ws.cleanupClients();
+
+  static unsigned long prevTime = 0;
+  if (millis() - prevTime >= 5000) {
+    sendMeasurementData();
+    prevTime = millis();
+  }
 }
