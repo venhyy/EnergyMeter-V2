dcb126d1dba53b9035e10f27c64b568511f1419e
Jaroslav Venhoda <venhy1337@gmail.com>
2023-04-08 13:17:15 +0200

    added OTA function

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <AsyncTCP.h>
 #include <ArduinoJson.h>
 #include <HardwareSerial.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 const char *ssid = "ASUS";
 const char *password = "asus1337";
@@ -115,6 +118,34 @@ void setup()
   server.addHandler(&ws);
   server.begin();
 
+  // Set up OTA update
+  ArduinoOTA.setHostname("energymeter"); // Set the hostname of your device
+
+  ArduinoOTA
+      .onStart([]()
+               {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+  ArduinoOTA.begin();
+
   for (int i = 0; i < MAX_PZEM; i++)
   {
     lastMeasurementTime[i] = 0;
@@ -126,10 +157,13 @@ void loop()
   // Handle WebSocket events
   ws.cleanupClients();
 
+  // OTA handler
+  ArduinoOTA.handle(); // Check for OTA updates
+
   static unsigned long prevTime = 0;
   if (millis() - prevTime >= 5000)
   {
-    StaticJsonDocument<256> doc;
+    StaticJsonDocument<300> doc;
     JsonArray arr = doc.to<JsonArray>();
 
     for (int i = 0; i < 3; i++)
